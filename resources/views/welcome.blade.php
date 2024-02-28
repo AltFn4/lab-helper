15cfe8acbe16f2b3498b4a8969d1f29fe275e667
AltFn4 <116887922+AltFn4@users.noreply.github.com>
2024-02-28 21:19:46 +0800

    Updated welcome page UI. Added back to top button.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,76 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LabLink - Your Virtual Lab Assistant</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>LabLink - Your Virtual Lab Assistant</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased bg-gray-800">
-        <div class="px-5 w-screen h-screen relative">
-            <div class="absolute inset-x-0 top-0 z-10 w-screen">
-                <div class="grid grid-cols-2 justify-between">
-                    <h2 class="p-5 text-gray-300 text-xl">
-                        LabLink
-                    </h2>
-                    <div class="p-5 text-right">
-                        <a href="{{route('login')}}" class="p-5 text-gray-300 hover:animate-fade rounded">Login</a>
-                    </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="antialiased bg-gradient-to-r from-sky-500 to-indigo-500">
+    <div class="relative overflow-x-hidden">
+        <div class="top-0 z-10 w-screen">
+            <div class="grid grid-cols-2 justify-between">
+                <h2 class="p-5 text-gray-300 text-xl">
+                    LabLink
+                </h2>
+                <div class="p-5 text-right">
+                    <a href="{{ route('login') }}" class="p-5 text-gray-300 hover:animate-fade rounded">Login</a>
                 </div>
-                <hr>
             </div>
-            <div class="absolute inset-0 z-0 bg-gradient-to-r from-sky-500 to-indigo-500 w-screen"></div>
-            <div class="flex flex-col gap-5 absolute inset-y-1/2 z-10 w-screen">
-                <p class="text-center">
-                    <a class="text-3xl">
-                        Connect with your lab with
+            <hr>
+        </div>
+        <a id="top"></a>
+        <article class="snap-y snap-mandatory overflow-hidden">
+            <section class="snap-center snap-always h-128 box-border relative">
+                <div class="bg-uni bg-no-repeat bg-cover bg-center blur size-full"></div>
+                <div
+                    class="absolute w-full z-10 flex flex-col gap-5 justify-center top-1/2 left-0 right-0 ml-auto mr-auto">
+                    <p class="text-center">
+                        <a class="text-3xl">
+                            Connect with your lab with
+                        </a>
+                        <br>
+                        <a class="text-3xl text-gray-300">
+                            LabLink
+                            <a>
+                                <br>
+                                your trusted partner in productivity!
+                    </p>
+                </div>
+
+            </section>
+            <section class="snap-center snap-always h-128 box-border relative">
+                <div class="bg-happy_helper bg-no-repeat bg-cover bg-center blur size-full"></div>
+                <p class="absolute w-full z-10 gap-5 justify-center top-1/2 left-0 right-0 ml-auto mr-auto text-center">
+                    <a class="text-3xl text-gray-300">
+                        Fast Response Time
                     </a>
                     <br>
-                    <a class="text-3xl text-gray-300">
-                        LabLink
-                    <a>
-                    <br>
-                    your trusted partner in productivity!
+                    Sign off or Ask question with fast response time!
                 </p>
-            </div>
-        </div>
-    </body>
+            </section>
+            <section class="snap-center snap-always h-128 box-border relative">
+                <div class="bg-happy_student bg-no-repeat bg-cover bg-center blur size-full"></div>
+                <p class="absolute w-full z-10 justify-center top-1/2 left-0 right-0 ml-auto mr-auto text-center">
+                    <a class="text-3xl text-gray-300">
+                        Enhanced lab experience
+                    </a>
+                    <br>
+                    Provides you the better lab experience!
+                </p>
+            </section>
+        </article>
+        <a href="#top">Back to top</a>
+    </div>
+</body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,46 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     enabled: process.env.NODE_ENV === "production",
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
             backgroundImage: {
-                'welcome': "url('https://images.unsplash.com/photo-1707056790571-54d8612d6368?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                welcome:
+                    "url('https://images.unsplash.com/photo-1707056790571-54d8612d6368?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
             },
             colors: {
-                'regal-blue': '#243c5a',
+                "regal-blue": "#243c5a",
             },
             animation: {
-                fade: 'fadeIn .5s ease-in forwards',
+                fade: "fadeIn .5s ease-in forwards",
             },
-            keyframes:  theme => ({
-                'fadeIn': {
-                    '0%': { backgroundColor: theme('colors.transparent') },
-                    '100%': { backgroundColor: theme('colors.black')},
+            keyframes: (theme) => ({
+                fadeIn: {
+                    "0%": { backgroundColor: theme("colors.transparent") },
+                    "100%": { backgroundColor: theme("colors.black") },
                 },
             }),
+            height: {
+                128: "48rem",
+            },
+            backgroundImage: {
+                uni: "url('https://visitwalesimages.thedms.co.uk/propertypics/MW/vlarge/0926720__9.jpg')",
+                happy_student:
+                    "url('https://thumbs.dreamstime.com/b/happy-german-female-student-group-computer-science-students-happy-german-female-student-group-computer-science-176998474.jpg')",
+                happy_helper:
+                    "url('https://thumbs.dreamstime.com/b/positive-classmates-smiling-looking-screen-pc-learning-computer-science-classroom-happy-students-enjoying-263549346.jpg')",
+            },
         },
     },
 
