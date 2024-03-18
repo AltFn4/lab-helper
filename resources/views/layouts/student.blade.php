811525939987742815d64ebe36442b5fe5a03849
AltFn4 T <2143975@swansea.ac.uk>
2024-03-18 20:17:46 +0000

    Optimized the code in dashboard view.

---
+++ resources/views/layouts/student.blade.php
@@ -0,0 +1,61 @@
+<section class="grid grid-cols-3 auto-rows-auto gap-5">
+    <div class="p-4 flex flex-col gap-3 bg-gray-500 rounded col-span-2">
+        <p class="text-gray-100 text-lg">
+            Status
+        </p>
+
+        @if(Auth::user()->seat)
+        <p>
+            Lab: {{ Auth::user()->lab->room->name }}
+        </p>
+        <p>
+            Seat: {{ Auth::user()->seat->id }}
+        </p>
+        <form action="{{ route('seat.leave') }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <x-danger-button>
+                leave
+            </x-danger-button>
+        </form>
+        @else
+        <p>
+            Please select a lab and seat.
+        </p>
+        <form action="{{ route('labs.index') }}" method="GET">
+            @csrf
+            @method('GET')
+            <x-primary-button>
+                select
+            </x-primary-button>
+        </form>
+        @endif
+    </div>
+    <div class="p-4 flex flex-col gap-3 bg-gray-500 rounded ">
+        @if(Auth::user()->inquiry == NULL)
+        <p class="text-gray-100 text-lg">
+            Get help
+        </p>
+        <form action="{{ route('inquiry.edit') }}" method="GET">
+            @csrf
+            @method('GET')
+            <x-primary-button>
+                request
+            </x-primary-button>
+        </form>
+        @else
+        <p class="text-gray-100 text-lg">
+            Your request
+        </p>
+        <p>Time elapsed: {{ Auth::user()->inquiry->created_at->diffInMinutes(Carbon\Carbon::now()) }} mins</p>
+        <form action="{{ route('inquiry.show') }}" method="GET">
+            @csrf
+            @method('GET')
+            <input type="hidden" name="inquiry_id" value="{{ Auth::user()->inquiry->id }}">
+            <x-primary-button>
+                inspect
+            </x-primary-button>
+        </form>
+        @endif
+    </div>
+</section>
