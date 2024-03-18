243fbbcb0853203dcbbc81f0a0703a86313fbc4b
AltFn4 T <2143975@swansea.ac.uk>
2024-03-18 21:39:44 +0000

    Redesigned the UI interface for selecting lab. Changed the randomly generated module names. Changed LabController to return a sorted filtered list of labs.

--- resources/views/labs/lab.blade.php
+++ resources/views/labs/lab.blade.php
@@ -1,25 +1,39 @@
 <x-app-layout>
     @include('labs.partials.stage', ['stage' => 'lab'])
-    <div class="p-5 flex flex-col gap-5 items-center">
+    <div class="p-5 m-5 grid grid-cols-1 gap-5">
         <h1 class="text-white text-5xl">Select Lab</h1>
-        <form action="{{route('select.lab')}}" method="post">
+        <p class="text-gray-100">Select the lab you are located in.</p>
+
+        <input type="text" id="searchBar" class="rounded" onkeyup="search()" placeholder="search...">
+
+        @foreach ($labs as $lab)
+        <form action="{{route('select.lab')}}" method="post" id="lab-{{ $lab->id }}" class="lab">
             @csrf
             @method("POST")
-            @if(session('message'))
-            <p class="p-2 bg-red-600 text-gray-300 rounded">
-                {{ session('message') }}
-            </p>
-            @endif
-            <select name="id" size="5" class="p-5 m-5 bg-gray-700 rounded text-gray-100" required>
-                @foreach ($labs as $lab)
-                <option value="{{ $lab->id }}">
-                    {{ $lab->room->name }}
-                </option>
-                @endforeach
-            </select>
-            <x-primary-button>
-                Select
-            </x-primary-button>
+            <input type="hidden" class="moduleName" value="{{ $lab->module->name }}">
+            <input type="hidden" name="lab_id" value="{{ $lab->id }}">
+            <div class="p-5 bg-gray-700 text-gray-100 rounded">
+                <h2 class="text-3xl">{{ $lab->module->name }}</h2>
+                <p>Room: {{ $lab->room->name }}</p>
+                <p>Start time: {{ $lab->start_time }}</p>
+                <p>Duration: {{ $lab->duration }} hour{{ $lab->duration > 1 ? "s" : "" }}</p>
+                <x-primary-button>
+                    Select
+                </x-primary-button>
+            </div>
         </form>
+        @endforeach
+
+        <script>
+            function search() {
+                var searchBar = $('#searchBar');
+                var pattern = searchBar.val().toUpperCase();
+
+                $('.lab').each(function (_, lab) {
+                    var moduleName = $(this).children('.moduleName').val().toUpperCase();
+                    this.style.display = moduleName.includes(pattern) ? 'block' : 'none';
+                });
+            }
+        </script>
     </div>
 </x-app-layout>
