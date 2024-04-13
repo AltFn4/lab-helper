4a88e0576d61f9e33081218f93a783fd69ac74e4
AltFn4 T <2143975@swansea.ac.uk>
2024-04-13 20:58:28 +0000

    Changed application logo. Added clock logo. Removed old selected seat logo. Added logos to welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,73 +1,81 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
-        <title>LabLink - Your Virtual Lab Assistant</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>LabLink - Your Virtual Lab Assistant</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased bg-gradient-to-r from-sky-500 to-indigo-500">
-        <div class="relative overflow-x-hidden">
-            <div class="top-0 z-10 w-screen">
-                <div class="grid grid-cols-2 justify-between">
-                    <h2 class="p-5 text-gray-300 text-xl">
-                        LabLink
-                    </h2>
-                    <div class="p-5 text-right">
-                        <a href="{{route('login')}}" class="p-5 text-gray-300 hover:animate-fade rounded">Login</a>
-                    </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="antialiased bg-gradient-to-r from-sky-500 to-indigo-500">
+    <div class="overflow-x-hidden relative">
+        <div class="top-0 z-10 w-screen">
+            <div class="grid grid-cols-2 justify-between">
+                <h2 class="p-5 text-xl text-gray-300">
+                    LabLink
+                </h2>
+                <div class="p-5 text-right">
+                    <a href="{{route('login')}}" class="p-5 text-gray-300 rounded hover:animate-fade">Login</a>
                 </div>
-                <hr>
             </div>
-            <a id="top"></a>
-            <article class="snap-y snap-mandatory overflow-hidden">
-                <section class="snap-center snap-always h-128 box-border relative">
-                    <div class="bg-uni bg-no-repeat bg-cover bg-center blur h-128"></div>
-                    <div class="absolute w-full z-10 flex flex-col gap-5 justify-center top-1/2 left-0 right-0 ml-auto mr-auto">
-                      <p class="text-center">
+            <hr>
+        </div>
+        <a id="top"></a>
+        <article class="overflow-hidden snap-y snap-mandatory">
+            <section class="box-border relative snap-center snap-always h-128">
+                <div class="bg-center bg-no-repeat bg-cover blur bg-uni h-128"></div>
+                <div class="flex absolute right-0 left-0 top-1/2 z-10 flex-col gap-5 justify-center mr-auto ml-auto w-full">
+                    <p class="text-center">
                         <a class="text-3xl">
                             Connect with your lab with
                         </a>
                         <br>
                         <a class="text-3xl text-gray-300">
                             LabLink
-                        <a>
+                        </a>
                         <br>
                         your trusted partner in productivity!
-                      </p>
-                    </div>
+                    </p>
+                    <x-application-logo class="block w-auto h-20 text-gray-800 fill-current" />
+                </div>
 
-                </section>
-                <section class="snap-center snap-always h-128 box-border relative">
-                  <div class="bg-happy_helper bg-no-repeat bg-cover bg-center blur h-128"></div>
-                  <p class="absolute w-full z-10 gap-5 justify-center top-1/2 left-0 right-0 ml-auto mr-auto text-center">
-                        <a class="text-3xl text-gray-300">
-                            Fast Response Time
-                        </a>
-                        <br>
-                        Sign off or Ask question with fast response time!
-                  </p>
-                </section>
-                <section class="snap-center snap-always h-128 box-border relative">
-                  <div class="bg-happy_student bg-no-repeat bg-cover bg-center blur h-128"></div>
-                  <p class="absolute w-full z-10 justify-center top-1/2 left-0 right-0 ml-auto mr-auto text-center">
+            </section>
+            <section class="box-border relative snap-center snap-always h-128">
+                <div class="bg-center bg-no-repeat bg-cover blur bg-happy_helper h-128"></div>
+                <div class="flex absolute right-0 left-0 top-1/2 z-10 flex-col gap-5 justify-center mr-auto ml-auto w-full text-center">
                     <a class="text-3xl text-gray-300">
-                            Enhanced lab experience
-                        </a>
-                        <br>
-                        Provides you the better lab experience!
-                  </p>
-                </section>
-            </article>
-            <a href="#top">Back to top</a>
-        </div>
-    </body>
+                        Fast Response Time
+                    </a>
+                    <p>
+                        Sign off or Ask question with fast response time!
+                    </p>
+                    <x-clock-logo class="block w-auto h-20 text-gray-800 fill-current" />
+                </div>
+
+
+            </section>
+            <section class="box-border relative snap-center snap-always h-128">
+                <div class="bg-center bg-no-repeat bg-cover blur bg-happy_student h-128"></div>
+                <p class="absolute right-0 left-0 top-1/2 z-10 justify-center mr-auto ml-auto w-full text-center">
+                    <a class="text-3xl text-gray-300">
+                        Enhanced lab experience
+                    </a>
+                    <br>
+                    Provides you the better lab experience!
+                </p>
+            </section>
+        </article>
+        <a href="#top">Back to top</a>
+    </div>
+</body>
+
 </html>
